8682960439feb8c95d8cf38f8b463f894db4953a
yoga Thrinadh <ythrinadh123@gmail.com>
2019-06-04 08:26:59 +0530

    Adding all to converting static to dynamic

---
+++ dynamicdata.js
@@ -0,0 +1,19 @@
+users = ["Home", "Contact us", "Login", "Logout", "Help", "Sign Up"];
+datalinks = ["Lorem ipsum dolor sit",
+    "Amet consectetur",
+    "Praesent vel sem id",
+    "Curabitur hendrerit est",
+    "Sed a nulla urna"];
+datalinks2 = ["Marketing Strategy",
+    "Email and Newsletters",
+    "Social Media",
+    "Photography",
+    "Video Web"]
+datalinks3 = ["Overview and Leadership",
+    "News and Public Relations",
+    "Creative Services",
+    "Web Strategy and E-Commerce",
+    "Marketing Strategy"]
+datalinks4 = ["Campaign Strategy",
+    "Database and Information", "ManagementEvent", "Registration Management",
+    "Customer Service and Distribution"]
